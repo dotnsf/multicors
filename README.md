666e05c6603b8e5550576e9377fcdbc8100c8f62
K.Kimura <dotnsf@gmail.com>
2022-05-22 11:47:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Multi CORS
+
+
+## Overview
+
+複数のオリジンに対応する CORS 実装
+
+
+## Environment Values
+
+- `CORS`
+
+  - REST API( `GET /ping` ) へのクロスアクセスを許可するオリジンをカンマ区切りで指定
+
+    - 例１: CORS=http://localhost:8000,https://xxxx.herouapp.com
+
+    - 例２: CORS=*
+
+
+## Licensing
+
+This code is licensed under MIT.
+
+
+## Copyright
+
+2022 [K.Kimura @ Juge.Me](https://github.com/dotnsf) all rights reserved.
---
+++ app.js
@@ -0,0 +1,41 @@
+//. app.js
+var express = require( 'express' ),
+    app = express();
+
+app.use( express.Router() );
+
+var settings_cors = 'CORS' in process.env ? process.env.CORS : '';  //. "http://localhost:8080,https://xxx.herokuapp.com"
+app.all( '/*', function( req, res, next ){
+  if( settings_cors ){
+    var origin = req.headers.origin;
+    if( origin ){
+      var cors = settings_cors.split( " " ).join( "" ).split( "," );
+
+      //. cors = [ "*" ] への対応が必要
+      if( cors.indexOf( '*' ) > -1 ){
+        res.setHeader( 'Access-Control-Allow-Origin', '*' );
+        res.setHeader( 'Vary', 'Origin' );
+      }else{
+        var idx = cors.indexOf( origin );
+        if( idx > -1 ){
+          res.setHeader( 'Access-Control-Allow-Origin', cors[idx] );
+          res.setHeader( 'Vary', 'Origin' );
+        }
+      }
+    }
+  }
+  next();
+});
+
+app.get( '/ping', function( req, res ){
+  res.contentType( 'application/json; charset=utf-8' );
+
+  res.write( JSON.stringify( { status: true, message: 'PONG' }, null, 2 ) );
+  res.end();
+});
+
+var port = process.env.PORT || 8080;
+app.listen( port );
+console.log( "server starting on " + port + " ..." );
+
+module.exports = app;
